6964ad62cc27d734fbc3174a5bc04b51db3c1aed
Brent Vatne <brentvatne@gmail.com>
2020-05-07 23:29:19 -0700

    Example to demonstrate

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+
+// @ts-ignore: if you use typescript you need to add the extension to tsconfig
+import webview from "./example.webview";
 
 export default function App() {
+  let [contents, setContents] = React.useState("");
+
+  React.useEffect(() => {
+    async function loadContentsAsync() {
+      // Ensure it's on local file system rather than accessed over network
+      const asset = Asset.fromModule(webview);
+      await asset.downloadAsync();
+
+      // Now that it's on FileSystem we can read the data
+      const data = await FileSystem.readAsStringAsync(asset.localUri!);
+      setContents(data);
+    }
+
+    loadContentsAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{contents ? contents : "loading"}</Text>
     </View>
   );
 }
@@ -12,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
